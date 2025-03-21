30d98d84774e93da3b5addc19326a0cee938a9e3
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-03-22 00:37:31 +0600

    added Navbar section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="" />
-      <li>Home</li>
-      <li>Program</li>
-      <li>About Us</li>
-      <li>Campus</li>
-      <li>Testimonials</li>
-      <li>Contact Us</li>
+    <nav className="container">
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li> <button className="btn">Contact Us</button> </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
