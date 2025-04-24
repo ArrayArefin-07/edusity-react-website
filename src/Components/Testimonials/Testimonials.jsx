540564064a85b8586f3a75ffc582fcaad73ca62e
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-04-24 14:43:54 +0600

    Added Functionality for sliding forward & back in Testimonials section

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import './Testimonials.css'
-import next_icon from '../../assets/next-icon.png'
-import back_icon from '../../assets/back-icon.png'
-import user_1 from  '../../assets/user-1.png'
-import user_2 from '../../assets/user-2.png'
-import user_3 from '../../assets/user-3.png'
-import user_4 from '../../assets/user-4.png'
+import React, { useRef } from "react";
+import "./Testimonials.css";
+import next_icon from "../../assets/next-icon.png";
+import back_icon from "../../assets/back-icon.png";
+import user_1 from "../../assets/user-1.png";
+import user_2 from "../../assets/user-2.png";
+import user_3 from "../../assets/user-3.png";
+import user_4 from "../../assets/user-4.png";
 
 const Testimonials = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+  const slideBackward = () => {
+    if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+
   return (
-    <div className='testimonials'>
-      <img src={next_icon} alt="" className='next-btn'/>
-      <img src={back_icon} alt="" className='back-btn'/>
+    <div className="testimonials">
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
       <div className="slider">
-        <ul>
+        <ul ref={slider}>
           <li>
             <div className="slide">
               <div className="user-info">
                 <img src={user_1} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 1</h3>
                   <span>Edustry, USA</span>
                 </div>
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, illo. Suscipit similique corrupti doloremque sed atque dolorum maiores, voluptatem nesciunt neque, asperiores facilis consequatur alias.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, illo. Suscipit similique corrupti doloremque sed
+                atque dolorum maiores, voluptatem nesciunt neque, asperiores
+                facilis consequatur alias.
+              </p>
             </div>
           </li>
           <li>
@@ -31,11 +57,16 @@ const Testimonials = () => {
               <div className="user-info">
                 <img src={user_2} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 2</h3>
                   <span>Edustry, USA</span>
                 </div>
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, illo. Suscipit similique corrupti doloremque sed atque dolorum maiores, voluptatem nesciunt neque, asperiores facilis consequatur alias.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, illo. Suscipit similique corrupti doloremque sed
+                atque dolorum maiores, voluptatem nesciunt neque, asperiores
+                facilis consequatur alias.
+              </p>
             </div>
           </li>
           <li>
@@ -43,11 +74,16 @@ const Testimonials = () => {
               <div className="user-info">
                 <img src={user_3} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 3</h3>
                   <span>Edustry, USA</span>
                 </div>
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, illo. Suscipit similique corrupti doloremque sed atque dolorum maiores, voluptatem nesciunt neque, asperiores facilis consequatur alias.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, illo. Suscipit similique corrupti doloremque sed
+                atque dolorum maiores, voluptatem nesciunt neque, asperiores
+                facilis consequatur alias.
+              </p>
             </div>
           </li>
           <li>
@@ -55,19 +91,22 @@ const Testimonials = () => {
               <div className="user-info">
                 <img src={user_4} alt="" />
                 <div>
-                  <h3>William Jackson</h3>
+                  <h3>William Jackson 4</h3>
                   <span>Edustry, USA</span>
                 </div>
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, illo. Suscipit similique corrupti doloremque sed atque dolorum maiores, voluptatem nesciunt neque, asperiores facilis consequatur alias.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati, illo. Suscipit similique corrupti doloremque sed
+                atque dolorum maiores, voluptatem nesciunt neque, asperiores
+                facilis consequatur alias.
+              </p>
             </div>
           </li>
-          
         </ul>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
