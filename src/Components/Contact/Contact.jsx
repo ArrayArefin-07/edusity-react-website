f4ef421b86e766c38ede4a5f73fb4c72b4ce1f0f
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2025-04-26 00:49:19 +0600

    added style for left part of contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./Contact.css";
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
 
 const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-col">
-        <h3>Send us a message</h3>
+        <h3>Send us a message <img src={msg_icon} alt="" /> </h3>
         <p>
           Feel free to reach out through contact form or find our contact information below. Your feedback, question, and suggestion are important to us as we strive to provide exceptional services to our university community.
         </p>
         <ul>
-          <li>Contact@DevArefin.dev</li>
-          <li>+8801234567893</li>
-          <li>Main Road, Bashundhara R/A <br/> Vatara, Dhaka </li>
+          <li> <img src={mail_icon} alt="" /> Contact@DevArefin.dev</li>
+          <li> <img src={phone_icon} alt="" /> +8801234567893</li>
+          <li> <img src={location_icon} alt="" /> Main Road, Bashundhara R/A <br/> Vatara, Dhaka </li>
         </ul>
       </div>
       <div className="contact-col"></div>
